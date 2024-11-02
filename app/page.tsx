9e6bfc5f93e884fb111b46325a2005060153b938
ariesanhthu <anhthuxitin@outlook.com>
2024-11-02 23:16:23 +0700

    change

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,9 @@ export default async function Home() {
   return (
 
     <div className="w-full h-full p-10">
+
       {/* section MAIN WELCOME */}
+      
       <div className="w-full flex md:flex-row gap-5 items-center justify-center flex-col">
         <div className="flex w-full md:w-1/3 flex-col justify-items-center text-center md:text-left sm:max-2xl:gap-5 gap-2">
           <p className="xl:text-6xl xl:leading-snug sm:max-2xl:p-0 text-3xl pl-5 pr-5 sm:max-lg:text-4xl font-bold uppercase">{data.slogan}</p>
@@ -30,6 +32,7 @@ export default async function Home() {
         </div>
         <Slideshow images={data.images} count = {imagesCount}/>
       </div>
+      
       {/* section HOT TOUR */}
 
         <RegionTour/>
