7c667bc80994d2eada21e78e3700704ac4bb93de
unknown <anhthuxitin@outlook.com>
2025-02-25 23:20:19 +0700

    category completed!

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -7,6 +7,8 @@ import Image from "next/image";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { productProps } from "../interface";
+import { formatCurrency } from "@/utils/formatCurrency";
+import Link from "next/link";
 
 export default function ProductCategoryPage() {
   const { category } = useParams() as { category: string };
@@ -37,7 +39,7 @@ export default function ProductCategoryPage() {
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100 p-4">
       <h1 className="text-3xl font-bold mb-8 text-center">
-        Danh sách sản phẩm - {category}
+        Danh sách sản phẩm
       </h1>
 
       {loading && <p className="text-center">Đang tải dữ liệu...</p>}
@@ -45,6 +47,7 @@ export default function ProductCategoryPage() {
 
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {products.map((product) => (
+          <Link href={`/product/${product._id}`} className="block w-80">
           <Card
             key={product._id}
             className="bg-gray-800 shadow-lg rounded-lg overflow-hidden transition-transform hover:scale-[1.02]"
@@ -67,15 +70,16 @@ export default function ProductCategoryPage() {
               <p className="mt-2 text-sm text-gray-300 text-muted-foreground mb-4 line-clamp-2 h-[2.5em]">
                 {product.description}
               </p>
-              <p className="mt-4 font-bold text-lg">{product.price} ₫</p>
+              <p className="mt-4 font-bold text-lg text-right text-primary">{formatCurrency(product.price)}</p>
             </CardContent>
           </Card>
+          </Link>
         ))}
       </div>
 
       {/* Nếu muốn thêm button refresh hoặc load thêm, bạn có thể thêm ở đây */}
       <div className="mt-8 flex justify-center">
-        <Button onClick={() => window.location.reload()}>Tải lại</Button>
+        <Button onClick={() => window.location.reload()} className="text-white">Tải lại</Button>
       </div>
     </div>
   );
